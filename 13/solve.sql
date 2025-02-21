db8a811d4ff39758a481ed423b24483b0645522e
Scott Chacon <schacon@gmail.com>
2025-02-21 08:13:49 +0100

    day 13 try

--- 13/solve.sql
+++ 13/solve.sql
@@ -22,17 +22,25 @@ WITH RECURSIVE robot_moves AS (
       seconds + 1
   FROM robot_moves
   WHERE seconds < 100
+),
+distances AS (
+  -- for each robot, find the distace to the closest robot
+  SELECT
+    rm.id,
+    rm.seconds,
+    MIN(ABS(rm.x - rm2.x) + ABS(rm.y - rm2.y)) AS distance
+  FROM robot_moves rm
+  CROSS JOIN robot_moves AS rm2
+  WHERE rm.id != rm2.id
+  and rm.seconds = rm2.seconds
+  GROUP BY rm.id, rm.seconds
+  ORDER BY rm.id
 )
 
--- count robots in each quadrant
+select
+  seconds,
+  SUM(distance) as total_distance
+from distances
+group by seconds
+order by seconds;
 
-SELECT
-  SUM(CASE WHEN x < 101/2 AND y < 103/2 THEN 1 ELSE 0 END) 
-  * 
-  SUM(CASE WHEN x > 101/2 AND y < 103/2 THEN 1 ELSE 0 END)
-  *
-  SUM(CASE WHEN x < 101/2 AND y > 103/2 THEN 1 ELSE 0 END)
-  * 
-  SUM(CASE WHEN x > 101/2 AND y > 103/2 THEN 1 ELSE 0 END) AS total
-FROM robot_moves
-WHERE seconds = 100;
